96b1ca460e2ac634250fedf05b3c833557298fd6
Oakntk <oakoakku@hotmail.com>
2024-06-05 00:23:52 +0700

    first commit

---
+++ src/components/Portfolio/Data.jsx
@@ -0,0 +1,88 @@
+import Work1 from "../../assets/cryptopricetag.jpg";
+import Work2 from "../../assets/netflix-clone.jpg";
+import Work3 from "../../assets/automoodtracker.png";
+import Work4 from "../../assets/fitness-booking.jpg";
+import Work5 from "../../assets/socialmediaweb.PNG";
+import Work6 from "../../assets/terminus.PNG";
+import Work7 from "../../assets/cloudtime.PNG";
+import Work8 from "../../assets/certterminus.png";
+
+export const projectsData = [
+  {
+    id: 1,
+    image: Work1,
+    title: "Crypto Price Tracker",
+    category: "web",
+    description:
+      "A website for tracking realtime crypto price implemented by ReactJS and Ant Design",
+  },
+  {
+    id: 2,
+    image: Work2,
+    title: "Netflix clone",
+    category: "web",
+    description: "A Netflix clone website implemented by ReactJS and MUI",
+  },
+  {
+    id: 3,
+    image: Work3,
+    title: "Auto mood tracker",
+    category: "app",
+    description:
+      "A mood tracker web application implemented by ReactJS, MUI , Firebase and Dialogflow",
+  },
+  {
+    id: 4,
+    image: Work4,
+    title: "Fitness Booking",
+    category: "web",
+    description:
+      "A fitness booking website implemented by HTML , CSS , JS and Bootstrap",
+  },
+  {
+    id: 5,
+    image: Work5,
+    title: "Social media website",
+    category: "web",
+    description:
+      "A social media clone website implemented by ReactJS , css and unicons",
+  },
+  {
+    id: 6,
+    image: Work6,
+    title: "Terminus",
+    category: "web",
+    description:
+      "A company website implemented by ReactJS , Redux, Ant Design , Go , Laravel , MySQL",
+  },
+  {
+    id: 7,
+    image: Work7,
+    title: "Cloudtime Passenger",
+    category: "web",
+    description:
+      "A company website implemented by ReactJS , Redux, Ant Design , Go , Laravel , MySQL",
+  },
+
+  {
+    id: 8,
+    image: Work8,
+    title: "Certification ",
+    category: "app",
+    description:
+      "A company web app send notification to Line implemented by HTML , CSS, Bootstrap jQuery , Go , Firebase , MySQL",
+  },
+];
+
+// projects
+export const projectsNav = [
+  {
+    name: "all",
+  },
+  {
+    name: "web",
+  },
+  {
+    name: "app",
+  },
+];
---
+++ src/components/skills/Backend.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+const Backend = () => {
+  return (
+    <div className="skills__content">
+      <h3 className="skills__title">Backend Developer</h3>
+
+      <div className="skills__box">
+        <div className="skills__group">
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name">Go</h3>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name">Laravel</h3>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name">Postman</h3>
+            </div>
+          </div>
+        </div>
+
+        <div className="skills__group">
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name">MySQL</h3>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name">SQL</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Backend;
